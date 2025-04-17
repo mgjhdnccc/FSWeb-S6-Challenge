223c7355b326a35fe27a579269a5cdc49cbc28a9
5alikorkmaz <korkmaz65ali_@hotmail.com>
2025-04-17 15:41:52 +0300

    srythsr

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import StarWarsInput from './components/StarWarsInput';
 import React, { useEffect, useState } from 'react';
+import StarWarsInput from './components/StarWarsInput';
 import { Characters } from './components/Characters';
 import { StarWarsApi } from './api/starWarsApi';
 
 const App = () => {
   const [arama, setArama] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState({ results: [] });
 
   const handleChange = (e) => {
     setArama(e.target.value);
   };
 
-  useEffect(()=>{
-    StarWarsApi.getPoeple().then(result =>setList(result));
+  useEffect(() => {
+    StarWarsApi.getPeople().then(result => setList(result));
   }, []);
 
-    
   return (
     <div className="App">
       <h1 style={{ color: 'white' }}>Karakterler</h1>
-  
+
       <StarWarsInput value={arama} onChange={handleChange} />
-  
-      <p style={{ color: 'white' }}>Aranan kelime: {arama}</p>
+
+      <Characters charList={list} aramaKelimesi={arama} />
     </div>
   );
 };
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import { Character } from "./Character"
+import { Character } from "./Character";
 
-export const Characters = ({ charList }) => {
+export const Characters = ({ charList = { results: [] }, aramaKelimesi = '' }) => {
+  const filteredList = charList.results.filter(item =>
+    item.name.toLowerCase().includes(aramaKelimesi.toLowerCase())
+  );
 
-
-    return (
-        <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-            {
-                charList &&
-                charList.map(item => <Character key={item.url} charObj={item} /> )
-            }
-
-        </div>
-    )
-}
+  return (
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+      {
+        filteredList.map(item => (
+          <Character key={item.url} charObj={item} />
+        ))
+      }
+    </div>
+  );
+};
