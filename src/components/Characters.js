b3b65255fc4d22fd7fc176cd440dc872f3c5f443
5alikorkmaz <korkmaz65ali_@hotmail.com>
2025-04-17 17:50:39 +0300

    dgtukhj

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,17 +1,18 @@
 import { Character } from "./Character";
 
-export const Characters = ({ charList = [], aramaKelimesi = '' }) => {
-  const filteredList = charList.filter(item =>
-    item.name.toLowerCase().includes(aramaKelimesi.toLowerCase())
-  );
-
-  return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
-      {
-        filteredList.map(item => (
-          <Character key={item.url} charObj={item} />
-        ))
-      }
-    </div>
-  );
-};
+export const Characters = ({ charList = { results: [] }, aramaKelimesi = '' }) => {
+    const filteredList = charList.results.filter(item =>
+      item.name.toLowerCase().includes(aramaKelimesi.toLowerCase())
+    );
+  
+    return (
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
+        {
+          filteredList.map(item => (
+            <Character key={item.url} charObj={item} />
+          ))
+        }
+      </div>
+    );
+  };
+  
